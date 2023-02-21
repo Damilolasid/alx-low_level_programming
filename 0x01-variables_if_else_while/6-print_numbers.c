e69bf04358734c3e9a21d843d04a219f84c8e05b
Damilolasid <abdulkareemsiddiqat@outlook.com>
2023-02-21 15:40:04 -0800

    print-numbers

--- 0x01-variables_if_else_while/6-print_numbers.c
+++ 0x01-variables_if_else_while/6-print_numbers.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
  *main - Entry point
- *Description: 'print all digit numbers of base 10'
+ *Description: 'print numbers in base 10 starting from 0'
  *Return: Always 0 (Success)
  */
 int main(void)
 {
-int n;
-for (n = 0; n <= 9; n++)
-{
-putchar(n + 48);
-}
-putchar('\n');
-return (0);
+  int n;
+  for (n = 0; n <= 9; n++)
+    {
+      putchar(n + 48);
+    }
+  putchar('\n');
+  return (0);
 }
