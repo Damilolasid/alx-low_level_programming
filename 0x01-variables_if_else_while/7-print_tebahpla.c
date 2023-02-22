2b0b0f23c74a71784b7458afe9f8db424347befe
Damilolasid <abdulkareemsiddiqat@outlook.com>
2023-02-22 14:07:17 -0800

    lowercase reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-  int n = 122;
-  while (n >= 97)
-    {
-      putchar(n);
-      n--;
-    }
-  putchar('\n');
-  return (0);
+int n = 122;
+while (n >= 97)
+{
+putchar(n);
+n--;
+}
+putchar('\n');
+return (0);
 }
