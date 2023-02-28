15f24fab2985a4aa7b2b1416eb3605b3c0b87590
Damilolasid <abdulkareemsiddiqat@outlook.com>
2023-02-27 16:29:51 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 /**
 *  reset_to_98 - takes the pointer and updates the value
 *  @n: int to check
-*  Return: 0 is success
+*  Return: n
 */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 *n = 98;
 }
