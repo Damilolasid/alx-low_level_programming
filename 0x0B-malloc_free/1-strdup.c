2d6880e182c725cda66febfa5e8885f80fd8bd7e
Damilolasid <abdulkareemsiddiqat@outlook.com>
2023-03-14 00:05:49 +0000

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,11 +17,12 @@ if (str == NULL)
 return (NULL);
 while (str[length] != '\0')
 length++;
-duplicate = (char*)malloc((sizeof(char) * length) + 1);
+duplicate = (char *)malloc((sizeof(char) * length) +1);
 if (duplicate == NULL)
 return (NULL);
+ 
 for (i = 0;i < length; i++)
 duplicate[i] = str[i];
-duplicate[length] = '\0';
+duplicate[length] ='\0';
 return (duplicate);  
 }
